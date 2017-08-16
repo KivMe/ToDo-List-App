c2039597cd4b94491f469c8fb9dc4fdf1ddb8af1
KaloDFreelancer <dm.kaloyan@gmail.com>
2017-08-16 04:04:55 +0300

    Version 1.0
    Starting commit

---
+++ assets/js/todos.js
@@ -0,0 +1,44 @@
+$("ul").on("click", "li", function(){
+
+    $(this).toggleClass("grey");
+
+});
+
+$("ul").on("click","span", function(event){
+    
+    $(this).parent().fadeOut(500,function(){
+        
+        $(this).remove();
+    
+    });
+    
+    event.stopPropagation();
+
+});
+
+$("input[type = 'text']").on("keypress", function(event){
+    
+    if(event.which === 13){
+        
+        var todoText = $(this).val();
+        
+        $("ul").append("<li><span><i class=\"fa fa-trash\" aria-hidden=\"true\"></i></span> " + todoText + "</li>");
+        $(this).val("");
+    
+    }
+
+});
+
+$(".fa-plus").click(function(){
+    
+    if($("input[type = 'text']").css("display") == "none"){
+        
+        $("input[type = 'text']").fadeIn();
+    
+    }else {
+        
+        $("input[type = 'text']").fadeOut();
+    
+    }
+
+});
